bd6d92ea981a8081f66ef4a36155a58b6b3475db
SIB61 <sib.sustswe@gmail.com>
2021-11-27 17:34:35 +0600

    first

--- app/src/main/java/com/sib/fascommerce/Seller/ProductListActivity.java
+++ app/src/main/java/com/sib/fascommerce/Seller/ProductListActivity.java
@@ -3,18 +3,14 @@ package com.sib.fascommerce.Seller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
 import com.sib.fascommerce.R;
 
-public class SellerHome extends AppCompatActivity {
+public class ProductListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_seller_home);
-    }
-
-    public void goToAddNew(View view) {
+        setContentView(R.layout.activity_product_list);
     }
 }
