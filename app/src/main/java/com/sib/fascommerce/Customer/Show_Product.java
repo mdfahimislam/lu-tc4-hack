7a74c0bc12b46d2b4c8c3b0034a1b84fd366497c
Abid <ahmedabid345678@gmail.com>
2021-11-27 13:23:51 +0600

    Abid The Ahmed

---
+++ app/src/main/java/com/sib/fascommerce/Customer/Show_Product.java
@@ -0,0 +1,16 @@
+package com.sib.fascommerce.Customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sib.fascommerce.R;
+
+public class Show_Product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_product);
+    }
+}
