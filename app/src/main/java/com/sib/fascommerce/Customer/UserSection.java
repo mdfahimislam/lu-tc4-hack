d285226e7d1f5562021f3853ea0e0b117b4ad4eb
Abid <ahmedabid345678@gmail.com>
2021-11-27 13:04:22 +0600

    Abid The Ahmed

---
+++ app/src/main/java/com/sib/fascommerce/Customer/UserSection.java
@@ -0,0 +1,14 @@
+package com.sib.fascommerce.Customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserSection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_section);
+    }
+}
